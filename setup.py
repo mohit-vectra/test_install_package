965f4d47c60f5c7c96dec17321dead3e830b4180
Mohit-Vectra <103417604+mohitVec@users.noreply.github.com>
2024-05-08 11:36:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.9.13',
     ],
 )

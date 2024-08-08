b30280548140a506a97df9b3063361af79312459
Mohit-Vectra <103417604+mohitVec@users.noreply.github.com>
2024-08-08 11:27:52 +0530

    Update calculator.py

--- sample_calculator/calculator.py
+++ sample_calculator/calculator.py
@@ -1,5 +1,4 @@
-import numpy as np
-import sympy as sp
+
 class Calculator:
     def add(self, a, b):
         return a + b
@@ -14,15 +13,3 @@ class Calculator:
         if b == 0:
             raise ValueError("Cannot divide by zero")
         return a / b
-    def np_infor(self):
-        #print("Numpy version : ",np.__version__)
-        result = np.add(10, 20)
-        print(result)
-        return result
-
-    def symbolic_add(self, expr1, expr2):
-        # Use sympy to add two symbolic expressions
-        x, y = sp.symbols('x y')
-        result = sp.simplify(expr1 + expr2)
-        print("Symbolic add result: ", result)
-        return result
